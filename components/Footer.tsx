dc17656a2c17e5a515797c82260d13563423df24
thierry <thierry@bigsool.com>
2019-06-25 19:37:26 +0200

    Github and Linkedin icon in footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,13 +1,20 @@
 import {Component} from 'react';
 import * as React from 'react';
 import './css/footer';
+import Avatar from '@material-ui/core/Avatar';
+import Chip from '@material-ui/core/Chip';
 
 export default class Footer extends Component {
 
     render() {
 
         return <footer>
-            <p>Site fait en ReactJS, mon code dispo sur <a href="https://github.com/unchunka/test-reactjs">Github</a></p>
+            <a href="https://www.linkedin.com/in/thierry-sambussy-62461b146/" title="Mon profil Linkedin">
+                <img src="./doc/linkedin-logo.png" alt=""/>
+            </a>
+            <a href="https://github.com/unchunka/test-reactjs/" title="Mon code sur Github">
+                <img src="./doc/github-logo.png" alt=""/>
+            </a>
         </footer>
 
     }
